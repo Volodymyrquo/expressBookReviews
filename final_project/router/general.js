7ef89cdffdbb20cc66dbb5775dfb07186d193999
Volodymyr Basok <volodymyrquo@gmail.com>
2023-12-19 04:38:52 -0500

    added enpoint for author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,27 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbnBook = books[req.params.isbn]
+  return res.status(300).json(isbnBook);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const booksbyauthor = Object.keys(books).filter(book=>books[book]["author"]===req.params.author).map(el=>books[el])
+  return res.status(300).json({booksbyauthor});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: req.params.title});
 });
 
 //  Get book review
