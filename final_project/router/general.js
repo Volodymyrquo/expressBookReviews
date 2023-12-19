8d6b50674762f3250e4b72cb65955122ca86db11
Volodymyr Basok <volodymyrquo@gmail.com>
2023-12-19 05:09:08 -0500

    added reviews endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const e = require('express');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -12,34 +13,35 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   return res.status(300).json({books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
   const isbnBook = books[req.params.isbn]
   return res.status(300).json(isbnBook);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-const booksbyauthor = Object.keys(books).filter(book=>books[book]["author"]===req.params.author).map(el=>books[el])
+const booksbyauthor = Object.keys(books).filter(book=>books[book]["author"]===req.params.author).map(el=>{
+    delete books[el].author; 
+    return {isbn:el,...books[el]}})
   return res.status(300).json({booksbyauthor});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: req.params.title});
-});
+    const booksbytitle = Object.keys(books).filter(book=>books[book]["title"]===req.params.title).map(el=>{
+        delete books[el].title; 
+        return {isbn:el,...books[el]}})
+      return res.status(300).json({booksbytitle});
+      });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbnReview = books[req.params.isbn].reviews
+  return res.status(300).json(isbnReview);
 });
 
 module.exports.general = public_users;
