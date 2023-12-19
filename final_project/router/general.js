41fe323ef7a774ae51ef865c03d17d2f0ea6ef8a
Volodymyr Basok <volodymyrquo@gmail.com>
2023-12-19 09:18:37 -0500

    added promises and callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+let booksPromise = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+     resolve(books)   
+    }, 6000);
+})
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -20,31 +24,67 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register customer."});
 });
 
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(300).json({books});
-});
+
+public_users.get('/',  function(req, res){
+    booksPromise.then(books=>res.status(300).json({ books }))
+    .catch(error=>{
+         console.error('Error fetching data:', error.message);
+         return res.status(500).json({ error: 'Internal Server Error' });
+    })
+  });
+
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbnBook = books[req.params.isbn]
-  return res.status(300).json(isbnBook);
+public_users.get('/isbn/:isbn',async function(req, res) {
+    booksPromise.then(books=>{ 
+        const isbnBook = books[req.params.isbn];
+        return res.status(300).json(isbnBook);
+    })
+        .catch(error=>{
+            console.error('Error fetching data:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+       })
+   
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-const booksbyauthor = Object.keys(books).filter(book=>books[book]["author"]===req.params.author).map(el=>{
-    delete books[el].author; 
-    return {isbn:el,...books[el]}})
-  return res.status(300).json({booksbyauthor});
+public_users.get('/author/:author',async function (req, res) {
+    booksPromise.then(books=>{
+        const booksbyauthor = Object.keys(books)
+        .filter(book=>books[book]["author"]===req.params.author)
+        .map(el=>{
+       const {title,reviews}=books[el];
+        return {isbn:el,title,reviews}
+    })
+        return res.status(300).json({booksbyauthor});
+    
+    })
+    .catch(error=>{
+        console.error('Error fetching data:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+   })
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const booksbytitle = Object.keys(books).filter(book=>books[book]["title"]===req.params.title).map(el=>{
-        delete books[el].title; 
-        return {isbn:el,...books[el]}})
+    booksPromise.then(books=>{
+       const booksbytitle = Object.keys(books)
+       .filter(book=>books[book]["title"]===req.params.title)
+       .map(el=>{
+       const {author,reviews} = books[el];
+        return {isbn:el,author,reviews};
+    })
       return res.status(300).json({booksbytitle});
+
+    })
+    .catch(error=>{
+        console.error('Error fetching data:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+   })
+
       });
 
 //  Get book review
